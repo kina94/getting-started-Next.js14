99c65096505a088de351f4e0b08d9f213603ff2f
kina <chase245@naver.com>
2024-02-24 23:44:36 +0900

    [add] server side movies fetch

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -4,6 +4,16 @@ export const metadata: Metadata = {
   title: "Home",
 };
 
-export default function Page() {
-  return <div>Hello!</div>;
+const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
+
+async function getMovies() {
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+  const response = await fetch(URL);
+  const json = await response.json();
+  return json;
+}
+
+export default async function HomePage() {
+  const movies = await getMovies();
+  return <div>{JSON.stringify(movies)}</div>;
 }
