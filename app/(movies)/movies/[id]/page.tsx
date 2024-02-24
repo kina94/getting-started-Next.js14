db0c47a00c7d4f158bd357a8ed9ed71628b05296
kina <chase245@naver.com>
2024-02-25 00:34:45 +0900

    [add] suspense

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,7 +1,20 @@
-export default function MoveDetail({
+import { Suspense } from "react";
+import MovieInfo from "../../../../components/movie-info";
+import MovieVideos from "../../../../components/movie-videos";
+
+export default function MovieDetail({
   params: { id },
 }: {
   params: { id: string };
 }) {
-  return <h1>Movie {id}</h1>;
+  return (
+    <h1>
+      <Suspense fallback={<h1>Loading movie info</h1>}>
+        <MovieInfo id={id} />
+      </Suspense>
+      <Suspense fallback={<h1>Loading move video</h1>}>
+        <MovieVideos id={id} />
+      </Suspense>
+    </h1>
+  );
 }
